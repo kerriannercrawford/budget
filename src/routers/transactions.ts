6638a0b9b3742e4be3ac8040ee6eda5968b843e5
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-05 18:22:11 -0400

    Add transaction router paths

--- src/routers/transactions.ts
+++ src/routers/transactions.ts
@@ -3,8 +3,28 @@ import transactionsController from '../controller/transactions';
 
 const transactionsRouter = express.Router();
 
-transactionsRouter.get('/', (req, res) => {
+transactionsRouter.post('/', (req, res) => {
+  res.status(200).json({});
+})
+
+transactionsRouter.get('/:user_id', (req, res) => {
   res.status(200).json({});
 });
+
+transactionsRouter.get('/:user_id/:account_id', (req, res) => {
+  res.status(200).json({});
+});
+
+transactionsRouter.get('/:user_id/:transaction_id', (req, res) => {
+  res.status(200).json({});
+})
+
+transactionsRouter.patch('/:transaction_id', (req, res) => {
+  res.status(200).json({});
+})
+
+transactionsRouter.delete('/:user_id/:transaction_id', (req, res) => {
+  res.status(200).json({});
+})
 
 export default transactionsRouter;

0572a0f0696a5abbc4b9fc9e51708f90bdab03ce
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-05 18:16:36 -0400

    Group router paths

--- src/routers/groups.ts
+++ src/routers/groups.ts
@@ -3,8 +3,24 @@ import groupsController from '../controller/groups';
 
 const groupsRouter = express.Router();
 
-groupsRouter.get('/', (req, res) => {
+groupsRouter.post('/', (req, res) => {
+  res.status(200).json({});
+})
+
+groupsRouter.get('/:user_id', (req, res) => {
   res.status(200).json({});
 });
+
+groupsRouter.get('/:user_id/:group_id', (req, res) => {
+  res.status(200).json({});
+})
+
+groupsRouter.patch('/:group_id', (req, res) => {
+  res.status(200).json({});
+})
+
+groupsRouter.delete('/:user_id/:group_id', (req, res) => {
+  res.status(200).json({});
+})
 
 export default groupsRouter;

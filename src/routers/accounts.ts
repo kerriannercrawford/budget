07c0de8ba5e88a0a3c4f511713ef017e7ee7a604
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-05 18:12:19 -0400

    Account router paths

--- src/routers/accounts.ts
+++ src/routers/accounts.ts
@@ -3,8 +3,24 @@ import accountsController from '../controller/accounts';
 
 const accountsRouter = express.Router();
 
-accountsRouter.get('/', (req, res) => {
+accountsRouter.post('/', (req, res) => {
+  res.status(200).json({})
+})
+
+accountsRouter.get('/:user_id', (req, res) => {
   res.status(200).json({});
 });
+
+accountsRouter.get('/:user_id/:account_id', (req, res) => {
+  res.status(200).json({});
+});
+
+accountsRouter.patch('/:account_id', (req, res) => {
+  res.status(200).json({})
+});
+
+accountsRouter.delete('/:account_id', (req, res) => {
+  res.status(200).json({})
+})
 
 export default accountsRouter;

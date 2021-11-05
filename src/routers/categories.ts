7620ef37a99471f55c74472f14329798f7807920
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-05 18:14:47 -0400

    Category router paths

--- src/routers/categories.ts
+++ src/routers/categories.ts
@@ -3,8 +3,24 @@ import categoriesController from '../controller/categories';
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.get('/', (req, res) => {
+categoriesRouter.post('/', (req, res) => {
+  res.status(200).json({});
+})
+
+categoriesRouter.get('/:user_id', (req, res) => {
   res.status(200).json({});
 });
+
+categoriesRouter.get('/:user_id/:category_id', (req, res) => {
+  res.status(200).json({});
+})
+
+categoriesRouter.patch('/:category_id', (req, res) => {
+  res.status(200).json({});
+})
+
+categoriesRouter.delete('/:user_id/:category_id', (req, res) => {
+  res.status(200).json({});
+})
 
 export default categoriesRouter;

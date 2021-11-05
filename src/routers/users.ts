08637f6e4b7d326753f92c1c66713575df1e5ab1
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-05 18:24:34 -0400

    Add users router paths

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -3,8 +3,29 @@ import usersController from '../controller/users';
 
 const usersRouter = express.Router();
 
+
 usersRouter.get('/', (req, res) => {
   res.status(200).json({});
 });
+
+usersRouter.get('/:user_id', (req, res) => {
+  res.status(200).json({});
+})
+
+usersRouter.post('/login', (req, res) => {
+  res.status(200).json({});
+})
+
+usersRouter.post('/create', (req, res) => {
+  res.status(200).json({});
+})
+
+usersRouter.patch('/:user_id', (req, res) => {
+  res.status(200).json({});
+})
+
+usersRouter.delete('/:user_id', (req, res) => {
+  res.status(200).json({});
+})
 
 export default usersRouter;

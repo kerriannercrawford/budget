40fe755c33a99ec856f938dfe894ba6661e271bc
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-06 13:19:51 -0400

    group routes done

--- src/controller/groups.ts
+++ src/controller/groups.ts
@@ -1,16 +1,43 @@
 const db = require('../models/database');
+const queries = require('./queries/groups');
 
 const groupsController: any = {};
 
-groupsController.createGroup = async () => {};
+groupsController.createGroup = async (req: any, res: any, next: any) => {
+  const { user_id, name } = req.body;
+  const values = [user_id, name];
+  const group = await db.query(queries.createGroup, values);
+  res.locals.group = group.rows;
+  return next();
+};
 
-groupsController.getAllUserGroups = async () => {};
+groupsController.getAllUserGroups = async (req: any, res: any, next: any) => {
+  const groups = await db.query(queries.getAllUserGroups, [req.params.user_id]);
+  res.locals.groups = groups.rows;
+  return next();
+};
 
-groupsController.getOneUserGroup = async () => {};
+groupsController.getOneUserGroup = async (req: any, res: any, next: any) => {
+  const { user_id, group_id } = req.params;
+  const group = await db.query(queries.getOneUserGroup, [user_id, group_id]);
+  res.locals.group = group.rows;
+  return next();
+};
 
-groupsController.updateGroup = async () => {};
+groupsController.updateGroup = async (req: any, res: any, next: any) => {
+  const { queryString, values } = await queries.updateGroup(req.body, req.params.group_id);
+  const group = await db.query(queryString, values);
+  res.locals.group = group.rows;
+  return next();
+};
 
-groupsController.deleteGroup = async () => {};
+groupsController.deleteGroup = async (req: any, res: any, next: any) => {
+  console.log(queries.deleteGroup);
+  console.log(req.params.group_id);
+  const group = await db.query(queries.deleteGroup, [req.params.group_id]);
+  res.locals.group = group.rows;
+  return next();
+};
 
 
 module.exports = groupsController;
--- src/controller/queries/groups.ts
+++ src/controller/queries/groups.ts
@@ -1,4 +1,47 @@
 const queries: any = {};
 
+queries.createGroup = `
+INSERT INTO groups (user_id, name)
+VALUES ($1, $2)
+RETURNING _id, user_id, name`;
+
+queries.getAllUserGroups = `
+SELECT * FROM groups
+WHERE user_id=$1`;
+
+queries.getOneUserGroup = `
+SELECT * FROM groups
+WHERE user_id=$1 AND _id=$2`;
+
+queries.deleteGroup = 'DELETE FROM groups WHERE _id=$1 RETURNING _id';
+
+queries.updateGroup = async (body: any, group_id: string) => {
+  const keys = Object.keys(body);
+  const values = [group_id];
+
+  let queryString = `
+  UPDATE groups
+  SET `;
+
+  let returnString = ' WHERE _id = $1 RETURNING _id, user_id, name';
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const currentChunk = `${keys[i]} = $${i + 2}`;
+    queryString = queryString + currentChunk;
+    if (i + 1 < keys.length) {
+      queryString = queryString + ', '
+    }
+    values.push(body[keys[i]]);
+  };
+
+  queryString = queryString + returnString;
+
+  return {
+    queryString, 
+    values
+  }
+}
+
+module.exports = queries;
 
 export {}
